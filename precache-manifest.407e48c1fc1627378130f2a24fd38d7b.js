abe9187dfa589d243250ed0e7a936a5db9d8a79e
Nero <nerochen25@berkeley.edu>
2020-09-06 17:57:12 -0700

    Updates

---
+++ precache-manifest.407e48c1fc1627378130f2a24fd38d7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94c08332fce39db46b5b70b44a9cdbe",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "71f55461a88f54e6ea3f",
+    "url": "/covid19-tracker/static/css/main.3f77a617.chunk.css"
+  },
+  {
+    "revision": "ee114967243ae6356510",
+    "url": "/covid19-tracker/static/js/2.8015c764.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.8015c764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f55461a88f54e6ea3f",
+    "url": "/covid19-tracker/static/js/main.9b04a612.chunk.js"
+  },
+  {
+    "revision": "eb5ff0e51105f2f9eece",
+    "url": "/covid19-tracker/static/js/runtime-main.782e1f46.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
